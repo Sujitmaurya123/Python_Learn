3b4de0c612a4321242c2edb32962e4984e3a37cc
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-02-28 00:31:33 +0530

    String in Python

---
+++ StringInPython/01_String.py
@@ -0,0 +1,29 @@
+myString="python"
+
+# print(myString[-2])# o
+# print(myString[:3])#pyt
+# print(myString[4:])#on
+# print(myString[:])#python
+
+# string methods
+
+
+# print(myString.find('h'))# 3
+# print(myString.count('t'))# 1
+# print(myString.lower())#python
+# print(myString.upper())#PYTHON
+# print(myString.replace('h','s'))#pytson
+
+
+# s="abcdef"
+
+# s=s[:3]+"sk"+s[4:]
+
+# print(s)#abcskef
+
+mystr="sujitkumar"
+sk=sorted(mystr)
+print(sk)# ['a', 'i', 'j', 'k', 'm', 'r', 's', 't', 'u', 'u']
+
+
+
